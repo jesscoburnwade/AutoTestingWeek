b8f8aa3f95957495d98492a7453b68a15f4dbc9c
jesscoburnwade <coburn.jessica@btinternet.com>
2018-08-17 12:27:34 +0100

    assessment

---
+++ Assessment/src/main/java/org/jess/Assessment/App.java
@@ -0,0 +1,13 @@
+package org.jess.Assessment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Assessment/src/main/java/org/jess/Assessment/Constant.java
@@ -0,0 +1,8 @@
+package org.jess.Assessment;
+
+public class Constant {
+	static String loginURL = "https://qa-trials641.orangehrmlive.com/auth/login";
+	static String loggedInURL = "";
+	static String path = "C:\\Users\\Admin\\Desktop\\AutomationReports\\";
+	static String file = "TeaReport.html";
+}
